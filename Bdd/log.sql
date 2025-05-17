fe08f560ee2a20e3a44106105122f602b0c824f9
DylanG-Dev <gametdylan@gmail.com>
2025-05-17 17:23:40 +0200

    Update log.sql
    finalisation log.sql

--- Bdd/log.sql
+++ Bdd/log.sql
@@ -26,8 +26,3 @@ ENGINE=INNODB;
 INSERT INTO `loueur`(`idLoueur`, `nom`, `mot_de_passe`, `isAdmin`)
 VALUES ('2000','Admin','Admin', '1'),
        ('3000', 'Loueur', 'Loueur', '0');ALTER TABLE `loueur` ADD `motdepasse` VARCHAR(255);
-
-ALTER TABLE `loueur` ADD `pays` VARCHAR(255);
-ALTER TABLE `loueur` ADD `email` VARCHAR(255);
-ALTER TABLE `loueur` ADD `numTel` VARCHAR(255);
-ALTER TABLE `loueur` ADD `date` DATETIME DEFAULT CURRENT_TIMESTAMP;
